7f23861480c8fca0e07a3e399b8c49b3486b72c7
Alessandro Clocchiatti <aleclock@MacBook-Pro-di-Alessandro.local>
2020-05-13 19:28:42 +0200

    Starded esercizio1, finish the function that calculate the similarity beetween definition

---
+++ esercitazione1/main.py
@@ -0,0 +1,46 @@
+import csv
+from Similarity import Similarity
+from prettytable import PrettyTable
+
+# cd /Users/aleclock/Desktop/uni/TLN/dicaro/progettoTLN_DiCaro/esercitazione1
+
+def load_csv(path):
+    dataset = {
+        'conc_generic': [],
+        'conc_specific': [],
+        'abst_generic': [],
+        'abst_specific': []
+    }
+    with open(path, 'r') as fileCSV:
+        for row in fileCSV.readlines()[1:]:
+            temp = row.split(";")
+            temp[-1] = temp[-1].replace('\n', '')
+
+            dataset["conc_generic"].append(temp[1])
+            dataset["conc_specific"].append(temp[2])
+            dataset["abst_generic"].append(temp[3])
+            dataset["abst_specific"].append(temp[4])
+
+    return dataset
+
+def main():
+    definizioni = load_csv('./definizioni00.csv')
+    similarity = {
+        'conc_generic': [],
+        'conc_specific': [],
+        'abst_generic': [],
+        'abst_specific': []
+    }
+    s = Similarity()
+    for d in definizioni:
+        dProcessed = s.preProcess(definizioni[d])
+        vSimilarity = s.getSimilarity(dProcessed)
+        similarity[d].append(vSimilarity)
+
+    table = PrettyTable()
+    table.field_names = ["", "Abstract", "Concrete"]
+    table.add_row(["Generic" , similarity["abst_generic"][0], similarity["conc_generic"][0]])
+    table.add_row(["Specific", similarity["abst_specific"][0], similarity["conc_specific"][0]])
+    print(table)
+
+main()
